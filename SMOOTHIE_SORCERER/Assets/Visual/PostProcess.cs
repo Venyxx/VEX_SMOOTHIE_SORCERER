85f7495f66fb593d9e2c691187ffc122f3125565
Brendan <weegeegotme@gmail.com>
2022-10-29 21:07:30 -0400

    Added the latest models
    Added the Scroll, Added the crystal ball, added the broom, Added the Staff

--- SMOOTHIE_SORCERER/Assets/Visual/PostProcess.cs
+++ SMOOTHIE_SORCERER/Assets/Visual/PostProcess.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class PostProcess : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Material mat;
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Graphics.Blit(source, destination, mat);
     }
 }
